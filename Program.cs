04ded2e269dd6bc2b2cb44ed759e60fcbb6d07d1
Santos Odigie <santosodigie104@gmail.com>
2024-10-17 10:29:33 +0100

    Init repo Created logic for user and cpu rolling dice

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, Welcome to my simple dice game!");
+Random rnd = new Random();
+bool exit = false;
+
+while (!exit)
+{
+    Console.WriteLine("Press any button to roll the dice");
+    Console.ReadKey();
+
+    int user = rnd.Next(1, 7);
+    int cpu = rnd.Next(1, 7);
+    Console.WriteLine($"\nYou rolled {user}");
+    Console.WriteLine($"\nThe cpu rolled {cpu}");
+    Console.WriteLine("_________________________________");
+}
